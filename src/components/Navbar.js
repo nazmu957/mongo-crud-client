db0c2c14da07211bc58e4b82d5b6491565acd0fc
nazmu957 <nazmus957@gmail.com>
2023-03-27 14:14:34 +0600

    navbar updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,14 +5,14 @@ const Navbar = () => {
         <div>
             <div className="navbar bg-neutral-300">
   <div className="flex-1">
-    <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+    <a className="btn btn-ghost normal-case text-xl">GadgetVilla</a>
   </div>
   <div className="flex-none">
     <ul className="menu menu-horizontal px-1">
-      <li><a>Item 1</a></li>
+      <li><a>Mobile</a></li>
       <li tabIndex={0}>
         <a>
-          Parent
+          Others
           <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
         </a>
         <ul className="p-2 bg-base-100">
@@ -20,7 +20,7 @@ const Navbar = () => {
           <li><a>Submenu 2</a></li>
         </ul>
       </li>
-      <li><a>Item 3</a></li>
+      <li><a>Laptops</a></li>
     </ul>
   </div>
 </div>
