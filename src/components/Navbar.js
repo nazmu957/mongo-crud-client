cba2a42864b42fb6188f082419cce9f18bc2d7c1
nazmu957 <nazmus957@gmail.com>
2023-03-30 15:25:30 +0600

    removing  the list item2

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -16,7 +16,7 @@ const Navbar = () => {
           <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
         </a>
         <ul className="p-2 bg-base-100">
-          <li><a>Mobile Components and devices</a></li>
+          <li><a>Mobile Components </a></li>
           <li><a>Laptop Components  and devices </a></li>
         </ul>
       </li>
