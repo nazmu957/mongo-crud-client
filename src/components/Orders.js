abe45ff73bf56e3a0374c665fa08a5930235b7fd
nazmu957 <nazmus957@gmail.com>
2023-04-04 13:54:46 +0600

    order deletede second part

--- src/components/Orders.js
+++ src/components/Orders.js
@@ -8,7 +8,7 @@ const Orders = () => {
             <h1>Order selected</h1>
             <h1>Order selected</h1>
             <h1>Order selected</h1>
-            <h1>Order selected</h1>
+            
             
             
          
